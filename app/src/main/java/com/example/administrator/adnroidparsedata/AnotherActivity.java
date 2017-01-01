1003ea8fe8e822a0442f83f09994347ccee99ee5
JasonBao-BB <371700894@qq.com>
2017-01-01 13:18:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/administrator/adnroidparsedata/AnotherActivity.java
@@ -0,0 +1,23 @@
+package com.example.administrator.adnroidparsedata;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AnotherActivity extends Activity {
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+
+        Intent intent = getIntent();
+        textView = (TextView) findViewById(R.id.dataGetTextView);
+        textView.setText(intent.getStringExtra("data"));
+
+    }
+
+
+}
