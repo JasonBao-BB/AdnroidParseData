00f87872ea7acf65b1c31f400d7bca41da63a5e3
JasonBao-BB <371700894@qq.com>
2017-01-01 15:40:47 -0500

    Different situation result

--- app/src/main/java/com/example/administrator/adnroidparsedata/AnotherActivity.java
+++ app/src/main/java/com/example/administrator/adnroidparsedata/AnotherActivity.java
@@ -5,10 +5,15 @@ import android.content.Intent;
 import android.os.Parcel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class AnotherActivity extends Activity {
     private TextView textView;
+    private Button goBack;
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,19 @@ public class AnotherActivity extends Activity {
         User user = intent.getParcelableExtra("User");
         textView = (TextView) findViewById(R.id.dataGetTextView);
         textView.setText(String.format("Name is %s, Age is %d",user.getName(),user.getPassword()));
+
+        editText = (EditText) findViewById(R.id.editText);
+
+        goBack = (Button) findViewById(R.id.goBack);
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backData = new Intent();
+                backData.putExtra("backData",editText.getText().toString());
+                setResult(1,backData);
+                finish();
+            }
+        });
     }
 
 
--- app/src/main/java/com/example/administrator/adnroidparsedata/MainActivity.java
+++ app/src/main/java/com/example/administrator/adnroidparsedata/MainActivity.java
@@ -7,24 +7,36 @@ import android.os.Bundle;
 import android.os.Parcel;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     Button next;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //next = (Button) findViewById(R.id.parseButton);
 
+        textView = (TextView) findViewById(R.id.dataFrom2nd);
+
         findViewById(R.id.parseButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,AnotherActivity.class);
                 intent.putExtra("User", new User("Bob",19));
-                startActivity(intent);
+                startActivityForResult(intent,0);
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == 1) {
+            textView.setText(data.getStringExtra("backData"));
+        }
     }
 }
